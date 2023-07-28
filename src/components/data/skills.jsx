4f5caec1144d31be8f8d52c581e516be2042635b
Ana <anabelenbernardez@gmail.com>
2023-07-28 13:27:34 +0200

    error className

--- src/components/data/skills.jsx
+++ src/components/data/skills.jsx
@@ -4,27 +4,27 @@ export const SkillsData = [
     list: [
       {
         name: "HTML5",
-        icon: <i class="devicon-html5-plain colored"></i>,
+        icon: <i className="devicon-html5-plain colored"></i>,
       },
       {
         name: "CSS3",
-        icon: <i class="devicon-css3-plain colored"></i>,
+        icon: <i className="devicon-css3-plain colored"></i>,
       },
       {
         name: "JavaScript",
-        icon: <i class="devicon-javascript-plain colored"></i>,
+        icon: <i className="devicon-javascript-plain colored"></i>,
       },
       {
         name: "ReactJS",
-        icon: <i class="devicon-react-original-wordmark colored"></i>,
+        icon: <i className="devicon-react-original-wordmark colored"></i>,
       },
       {
         name: "MySQL",
-        icon: <i class="devicon-mysql-plain-wordmark colored"></i>,
+        icon: <i className="devicon-mysql-plain-wordmark colored"></i>,
       },
       {
         name: "MySQL",
-        icon: <i class="devicon-nodejs-plain-wordmark colored"></i>,
+        icon: <i className="devicon-nodejs-plain-wordmark colored"></i>,
       },
     ],
   },
@@ -33,47 +33,47 @@ export const SkillsData = [
     list: [
       {
         name: "VSC",
-        icon: <i class="devicon-vscode-plain-wordmark colored"></i>,
+        icon: <i className="devicon-vscode-plain-wordmark colored"></i>,
       },
       {
         name: "Postman",
-        icon: <i class="devicon-vscode-plain-wordmark colored"></i>,
+        icon: <i className="devicon-vscode-plain-wordmark colored"></i>,
       },
       {
         name: "Scrum",
-        icon: <i class="devicon-vscode-plain-wordmark colored"></i>,
+        icon: <i className="devicon-vscode-plain-wordmark colored"></i>,
       },
       {
         name: "Git",
-        icon: <i class="devicon-git-plain-wordmark colored"></i>,
+        icon: <i className="devicon-git-plain-wordmark colored"></i>,
       },
       {
         name: "GitHub",
-        icon: <i class="devicon-github-original-wordmark colored"></i>,
+        icon: <i className="devicon-github-original-wordmark colored"></i>,
       },
       {
         name: "GitLab",
-        icon: <i class="devicon-gitlab-plain-wordmark colored"></i>,
+        icon: <i className="devicon-gitlab-plain-wordmark colored"></i>,
       },
       {
         name: "npm",
-        icon: <i class="devicon-npm-original-wordmark colored"></i>,
+        icon: <i className="devicon-npm-original-wordmark colored"></i>,
       },
       {
         name: "Photoshop",
-        icon: <i class="devicon-photoshop-plain colored"></i>,
+        icon: <i className="devicon-photoshop-plain colored"></i>,
       },
       {
         name: "Slack",
-        icon: <i class="devicon-slack-plain-wordmark colored"></i>,
+        icon: <i className="devicon-slack-plain-wordmark colored"></i>,
       },
       {
         name: "Trello",
-        icon: <i class="devicon-trello-plain-wordmark colored"></i>,
+        icon: <i className="devicon-trello-plain-wordmark colored"></i>,
       },
       {
         name: "Canva",
-        icon: <i class="devicon-canva-original colored"></i>,
+        icon: <i className="devicon-canva-original colored"></i>,
       },
     ],
   },
--- src/components/data/social.jsx
+++ src/components/data/social.jsx
@@ -1,17 +1,17 @@
 export const SocialData = [
   {
     platform: "Github",
-    icon: <i class="devicon-github-original"></i>,
+    icon: <i className="devicon-github-original"></i>,
     link: "https://github.com/AnaBelenBernardez",
   },
   {
     platform: "Linkedin",
-    icon: <i class="devicon-linkedin-plain"></i>,
+    icon: <i className="devicon-linkedin-plain"></i>,
     link: "https://www.linkedin.com/in/anabelenbernardez/",
   },
   {
     platform: "e-mail",
-    icon: <i class="devicon-linkedin-plain"></i>,
+    icon: <i className="devicon-linkedin-plain"></i>,
     link: "mailto:anabelenbernardez@gmail.com",
   },
 ];
