f1e478880502d46808c429a895fb0cb52b2af530
Ana <anabelenbernardez@gmail.com>
2024-04-01 20:57:38 +0200

    more responsive

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -3,12 +3,14 @@ import "./style.css";
 import { useTranslation } from "react-i18next";
 import Contact from "../Contact";
 import PhotosAbout from "./PhotosAbout";
+import ScrollToTop from "../details/ScrollToTop";
 
 function AboutPage() {
   const { t } = useTranslation();
 
   return (
     <main className="aboutMain">
+      <ScrollToTop />
       <div className="about">
         <p className="textAbout">{t("translation.about")}</p>
       </div>
