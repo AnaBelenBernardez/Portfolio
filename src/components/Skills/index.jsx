8dd9440ea684417959b20cc0333ffa6166af0658
Ana <anabelenbernardez@gmail.com>
2024-04-02 20:28:12 +0200

    Responsive for projects

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -3,43 +3,12 @@ import "./style.css";
 import { SkillsData } from "../data/skills.jsx";
 import SkillCard from "./Skillcard";
 import { useTranslation } from "react-i18next";
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-} from "pure-react-carousel";
 
 function Skills() {
   const data = SkillsData;
   const { t } = useTranslation();
 
   return (
-    // <CarouselProvider
-    //   totalSlides={data.length}
-    //   naturalSlideWidth={40}
-    //   naturalSlideHeight={45}
-    //   infinite="true"
-    //   interval={1000}
-    //   isPlaying={true}
-    //   visibleSlides={4}
-    //   id="carousel"
-    //   orientation="vertical"
-    // >
-    //   <div className="slider-container">
-    //     <Slider className="slider">
-    //       {data.map((skill, index) => (
-    //         <Slide index={index} key={index}>
-    //           <SkillCard skill={skill} />
-    //         </Slide>
-    //       ))}
-    //     </Slider>
-    //     <ButtonBack id="buttonback">Back</ButtonBack>
-    //     <ButtonNext id="buttonnext">Next</ButtonNext>
-    //   </div>
-    // </CarouselProvider>
-
     <div className="skills-container">
       {data.map((skill, index) => (
         <div className="skill-item" key={index}>
