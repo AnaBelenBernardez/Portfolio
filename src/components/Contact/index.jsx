1c2f6c07f39a5c96f72fc205c82c4e8be5511166
Ana <anabelenbernardez@gmail.com>
2024-04-03 18:22:48 +0200

    responsive about and contact-me

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -30,7 +30,7 @@ function Contact() {
       setCurrentPhraseIndex((prevIndex) =>
         prevIndex === phrases.length - 1 ? 0 : prevIndex + 1
       );
-    }, 4000);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, [phrases]);
@@ -38,25 +38,27 @@ function Contact() {
   return (
     <div className="contact">
       <p>{phrases[currentPhraseIndex]}</p>
-      <div className="socials">
-        <a
-          target="blank"
-          title="GitHub"
-          href="https://github.com/AnaBelenBernardez"
-        >
-          <i className="devicon-github-original"></i>
-        </a>
-        <a
-          target="blank"
-          title="LinkedIn"
-          href="https://www.linkedin.com/in/anabelenbernardez/"
-        >
-          <i className="devicon-linkedin-plain"></i>
+      <div className="contact-me">
+        <div className="socials">
+          <a
+            target="blank"
+            title="GitHub"
+            href="https://github.com/AnaBelenBernardez"
+          >
+            <i className="devicon-github-original"></i>
+          </a>
+          <a
+            target="blank"
+            title="LinkedIn"
+            href="https://www.linkedin.com/in/anabelenbernardez/"
+          >
+            <i className="devicon-linkedin-plain"></i>
+          </a>
+        </div>
+        <a href="mailto:anabelenbernardez@gmail.com" title="Send e-mail to Ana">
+          {t("translation.contactMe")}
         </a>
       </div>
-      <a href="mailto:anabelenbernardez@gmail.com" title="Send e-mail to Ana">
-        {t("translation.contactMe")}
-      </a>
     </div>
   );
 }
