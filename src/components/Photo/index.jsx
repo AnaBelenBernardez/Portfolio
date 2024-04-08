bccd5ff75d5f27549df13aa250b893771bb8586b
Ana <anabelenbernardez@gmail.com>
2024-04-08 20:54:04 +0200

    ive got some sparkles near me

--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -3,13 +3,36 @@ import "./style.css";
 
 function Photo() {
   return (
-    <div className="photo">
-      <img
-        loading="lazy"
-        src="./assets/photo.png"
-        alt="Ana Belén"
-        title="Ana Belén"
-      />
+    <div className="photo-container">
+      <div className="photo">
+        <div className="photo-left">
+          <img
+            id="photo-star"
+            className="stars"
+            src="/assets/bling.svg"
+            alt="Star"
+          />
+          <img
+            id="photo-star"
+            className="stars"
+            src="/assets/bling.svg"
+            alt="Star"
+          />
+        </div>
+        <img
+          id="my-photo"
+          loading="lazy"
+          src="./assets/photo.png"
+          alt="Ana Belén"
+          title="Ana Belén"
+        />
+        <img
+          className="stars"
+          src="/assets/bling.svg"
+          alt="Star"
+          id="photo-right"
+        />
+      </div>
     </div>
   );
 }
